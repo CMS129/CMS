dc322440a0b06f49f469ba5b0837618493e47464
CMS129 <77884781+CMS129@users.noreply.github.com>
2021-02-18 18:32:00 +0800

    cms
    cms

--- app/tpl/admin/index.php
+++ app/tpl/admin/index.php
@@ -3,6 +3,10 @@
 <html lang="zh">
 <head>
 <meta charset="utf-8">
+<meta http-equiv="x-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+<meta name="applicable-device" content="pc,mobile">
+<meta name="renderer" content="webkit|ie-comp|ie-stand">
 <title>{$title}</title>
 {include('_header.php')}
 
