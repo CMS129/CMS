a80260d53dc7cd2f5a9cb4c3bca782c19cd8c639
CMS129 <77884781+CMS129@users.noreply.github.com>
2021-02-18 18:18:32 +0800

    cms
    cms

--- app/tpl/admin/login.php
+++ app/tpl/admin/login.php
@@ -23,7 +23,7 @@
 						<form action="{$site_url}/login" method="post">
 							<div class="form-group">
 								<label class="form-control-label">用户名</label>
-								<input type="text" class="form-control" name="loginname" value="" tabindex="1" autocomplete="off" _input="true" placeholder="用户名或邮箱">
+								<input type="text" class="form-control" name="loginname" value="" tabindex="1" autocomplete="off" _input="true" placeholder="用户名">
 							</div>
 
 							<div class="form-group">
